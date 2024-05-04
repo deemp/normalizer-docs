8919168bb67967eb9b2e05c3951689ccc589fc41
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-04 15:40:00 +0300

    feat(flake): init

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell.url = "github:deemp/devshell";
+  };
+
+  outputs =
+    inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [ inputs.devshell.flakeModule ];
+      perSystem =
+        {
+          self',
+          system,
+          lib,
+          config,
+          pkgs,
+          ...
+        }:
+        {
+          packages.default = import ./nix/structurizr-lite.nix { inherit pkgs; };
+
+          devshells.default = {
+            commands = {
+              tools = [
+                pkgs.graphviz
+                pkgs.poetry
+              ];
+            };
+          };
+        };
+    };
+}
