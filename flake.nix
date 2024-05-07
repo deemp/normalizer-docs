e2e6a5195f0e6737418467700e91c48bc9c804d1
Danila Danko <br4ch1st0chr0n3@gmail.com>
2024-05-08 00:51:16 +0300

    feat(analytics): set up dev env

--- flake.nix
+++ flake.nix
@@ -24,6 +24,9 @@
           packages.default = import ./nix/structurizr-lite.nix { inherit pkgs; };
 
           devshells.default = {
+            bash.extra = ''
+              export LD_LIBRARY_PATH=${lib.makeLibraryPath [ pkgs.stdenv.cc.cc.lib ]}
+            '';
             commands = {
               tools = [
                 pkgs.graphviz
